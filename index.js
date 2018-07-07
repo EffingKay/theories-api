3a655d8fad02c31db17d9a26bd4097d7d3f6d2e6
EffingKay <klaudia@rozgonyiova.com>
2018-07-07 21:29:49 +0100

    cors issues

--- index.js
+++ index.js
@@ -33,6 +33,88 @@ function jwtErrorHandler(err, req, res, next){
 }
 
 app.use('/', routes);
-app.use(cors());
+
+var isPreflight = function(req) {
+    var isHttpOptions = req.method === 'OPTIONS';
+    var hasOriginHeader = req.headers['origin'];
+    var hasRequestMethod = req.headers['access-control-request-method'];
+    return isHttpOptions && hasOriginHeader && hasRequestMethod;
+  };
+  
+//   var createWhitelistValidator = function(whitelist) {
+//     return function(val) {
+//       for (var i = 0; i < whitelist.length; i++) {
+//         if (val === whitelist[i]) {
+//           return true;
+//         }
+//       }
+//       return false;
+//     }
+//   };
+  
+//   var originWhitelist = [
+//     'null',
+//     '*'
+//   ];
+  
+  var corsOptions = {
+    allowOrigin: '*',
+    allowCredentials: true,
+    shortCircuit: true,
+    allowMethods: ['GET', 'DELETE', 'PUT', 'PATCH', 'POST'],
+    allowHeaders: ['Timezone-Offset'],
+    maxAge: 120,
+    exposeHeaders: ['X-Powered-By']
+  };
+  
+  var handleCors = function(options) {
+    return function(req, res, next) {
+  
+      if (options.allowOrigin) {
+        var origin = req.headers['origin'];
+        if (options.allowOrigin(origin)) {
+          res.set('Access-Control-Allow-Origin', origin);
+        } else if (options.shortCircuit) {
+          res.status(403).end();
+          return;
+        }
+        res.set('Vary', 'Origin');
+      } else {
+        res.set('Access-Control-Allow-Origin', '*');
+      }
+  
+      if (options.allowCredentials) {
+        res.set('Access-Control-Allow-Credentials', 'true');
+      }
+  
+      if (isPreflight(req)) {
+        if (options.allowMethods) {
+          res.set('Access-Control-Allow-Methods',
+              options.allowMethods.join(','));
+        }
+        if (typeof(options.allowHeaders) === 'function') {
+          var headers = options.allowHeaders(req);
+          if (headers) {
+            res.set('Access-Control-Allow-Headers', headers);
+          }
+        } else if (options.allowHeaders) {
+          res.set('Access-Control-Allow-Headers',
+              options.allowHeaders.join(','));
+        }
+        if (options.maxAge) {
+          res.set('Access-Control-Max-Age', options.maxAge);
+        }
+        res.status(204).end();
+        return;
+      } else if (options.exposeHeaders) {
+        res.set('Access-Control-Expose-Headers', options.exposeHeaders.join(','));
+      }
+      next();
+    }
+  };
+
+app.use(handleCors(corsOptions));  
+
+// app.use(cors());
 
 app.listen(config.port, () => console.log(`Server has started on port ${config.port}`));
